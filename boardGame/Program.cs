d8665b3fc9a87bbd52be44bba5eda34c8cb568e3
Szymon radomski <sr@com.pl>
2024-12-03 08:55:56 +0100

    klasa player

--- boardGame/Program.cs
+++ boardGame/Program.cs
@@ -1,5 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+public class Player
+{
+    public string Name { get; set; }
+    public int Position { get; set; }
+    public int Score { get; set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        Position = 0; 
+        Score = 0; 
+    }
+
+    public int Ruch(int x)
+    {
+        Position += x;
+        return Position;
+    }
+
+    public int Aktualizacja(int score)
+    {
+        Score += score;
+        return Score;
+    }
+}
+
+
+
+
+
+
+
+
 internal class Program
 {
     public static void Main(string[] args)
