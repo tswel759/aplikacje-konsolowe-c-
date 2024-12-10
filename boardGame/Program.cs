980bbee57fa92c57037320e408a061130ccb5bf0
Szymon radomski <sr@com.pl>
2024-12-10 08:57:54 +0100

    Klasa Board

--- boardGame/Program.cs
+++ boardGame/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
 public class Player
 {
@@ -9,16 +9,16 @@ public class Player
     public Player(string name)
     {
         Name = name;
-        Position = 0; 
-        Score = 0; 
+        Position = 0;
+        Score = 0;
     }
-
+    
     public int Ruch(int x)
     {
         Position += x;
         return Position;
     }
-
+    
     public int Aktualizacja(int score)
     {
         Score += score;
@@ -28,21 +28,62 @@ public class Player
 
 public class Board
 {
-    public int RozmiarPl;
+    public int RozmiarPl { get; set; } 
+    public int[] Nagrody { get; set; } 
+    
+    public Board(int rozmiar)
+    {
+        RozmiarPl = rozmiar;
+        Nagrody = new int[rozmiar];
+        LosujNagrody();
+    }
+
+    
+    public void LosujNagrody()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < RozmiarPl; i++)
+        {
+            
+            Nagrody[i] = rand.Next(0, 5); 
+        }
+    }
+
+   
+    public int PobierzNagrode(int pozycja)
+    {
+      
+        if (pozycja >= 0 && pozycja < RozmiarPl)
+        {
+            return Nagrody[pozycja];
+        }
+        else
+        {
+            return 0; 
+        }
+    }
 }
-
-
-
-
-
-
 
 internal class Program
 {
     public static void Main(string[] args)
     {
+        
         Player player = new Player("RL9");
+        
+        Board board = new Board(20);
+        
         Console.WriteLine($"Gracz: {player.Name}, Pozycja: {player.Position}, Wynik: {player.Score}");
         
+        
+        int ruch = 5;
+        player.Ruch(ruch);
+        Console.WriteLine($"Po wykonaniu ruchu o {ruch}, Pozycja: {player.Position}");
+        
+        int nagroda = board.PobierzNagrode(player.Position);
+        Console.WriteLine($"Nagroda na polu {player.Position}: {nagroda}");
+        
+        player.Aktualizacja(nagroda);
+        Console.WriteLine($"Po aktualizacji, Wynik gracza: {player.Score}");
     }
 }
